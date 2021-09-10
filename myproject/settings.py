06eebf811664dae727f754569c30e02f2d6b6d40
umairny <umairny1@gmail.com>
2021-09-09 21:42:47 -0400

    static load

--- myproject/settings.py
+++ myproject/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
