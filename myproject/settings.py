f2b028ae0a7f121c091e334b1432a4357a650fd5
umairny <umairny1@gmail.com>
2021-09-09 21:33:26 -0400

    add heroku to settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -137,3 +138,4 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+django_heroku.settings(locals())
